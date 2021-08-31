f125dfdd9c3dcd07007fd75e39eefb07069e896b
Kyle Bouchard <kyle.bouchard@gremlin.com>
2021-08-31 15:56:08 -0400

    Added Gremlin ALFI code

---
+++ src/main/java/works/weave/socks/orders/config/FeatureFlag.java
@@ -0,0 +1,21 @@
+package works.weave.socks.orders.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Objects;
+
+@Configuration
+public class FeatureFlag {
+
+    private final Environment environment;
+
+
+    public FeatureFlag(Environment environment) {
+        this.environment = environment;
+    }
+
+    public boolean isAlfiEnabled() {
+        return  Objects.requireNonNull(environment.getProperty("GREMLIN_ALFI_ENABLED")).equalsIgnoreCase("true");
+    }
+}
